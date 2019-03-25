86a6eedf9041c3c5b73e6ffc852c25fbfd00e72d
Thomas Legrand <legrand.thomas5@hotmail.fr>
2019-03-25 22:34:24 +0100

    logit

--- src/main/kotlin/tech/thomaslegrand/kotlinml/Main.kt
+++ src/main/kotlin/tech/thomaslegrand/kotlinml/Main.kt
@@ -1,15 +1,19 @@
 package tech.thomaslegrand.kotlinml
 
 import krangl.*
-import smile.projection.PCA
 import smile.classification.LogisticRegression
+import smile.validation.Validation.cv
 
 fun main() {
     val iris = DataFrame.readCSV("src/main/resources/iris.csv")
 
+    println(iris.schema())
+
     val y = iris["species"].toIntArray()
     val X = iris.remove("species")
 
-    val pca = PCA(X.toDoubleMatrix())
-    val logit = LogisticRegression(X.toDoubleMatrix().transpose(), y)
+    val logit = LogisticRegression(X.toDoubleMatrix().transpose(), y, 0.0, 0.001, 500)
+
+    val test = arrayOf(3.1, 2.9, 1.7, 4.1)
+    println(logit.predict(test.toDoubleArray()))
 }
